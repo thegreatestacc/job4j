8a68f5fb94c6b0703d20792a8047803860962567
sovliv4git <sovliv4git@yandex.ru>
2019-01-22 22:05:05 +0300

    поправил по чекстайлу

--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -9,14 +9,14 @@ public class MaxTest {
     @Test
     public void whenFirstLessSecond() {
         Max max = new Max();
-        int result = max.max(1,4);
+        int result = max.max(1, 4);
         assertThat(result, is(4));
     }
 
     @Test
     public void whenSecondLessFirst() {
         Max max = new Max();
-        int result = max.max(3,2);
+        int result = max.max(3, 2);
         assertThat(result, is(3));
     }
 }
