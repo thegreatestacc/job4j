bb63aeff6bef299bf070f3efe4155f1adb2bc6f2
sovliv4git <sovliv4git@yandex.ru>
2019-01-22 22:03:29 +0300

    добавлен класс Max и MaxTest

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,10 @@
+package ru.job4j.max;
+
+public class Max {
+
+    int result;
+
+    public int max(int first, int second) {
+       return result = first > second ? first : second;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.max;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MaxTest {
+
+    @Test
+    public void whenFirstLessSecond() {
+        Max max = new Max();
+        int result = max.max(1,4);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void whenSecondLessFirst() {
+        Max max = new Max();
+        int result = max.max(3,2);
+        assertThat(result, is(3));
+    }
+}
