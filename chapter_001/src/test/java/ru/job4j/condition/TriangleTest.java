01bf21eff1c9b996ce23cff7a0d1ea18000d6800
sovliv4git <sovliv4git@yandex.ru>
2019-01-22 23:15:59 +0300

    выполнил задание по расчету площади треугольника

---
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.number.IsCloseTo.closeTo;
+import static org.junit.Assert.*;
+
+public class TriangleTest {
+    @Test
+    public void whenAreaSetThreePointsThenTriangleArea() {
+        // создаем три объекта класса Point.
+        MyPoint a = new MyPoint(0, 0);
+        MyPoint b = new MyPoint(0, 2);
+        MyPoint c = new MyPoint(2, 0);
+        // Создаем объект треугольник и передаем в него объекты точек.
+        Triangle triangle = new Triangle(a, b, c);
+        // Вычисляем площадь.
+        double result = triangle.area();
+        // Задаем ожидаемый результат.
+        double expected = 2D;
+        //Проверяем результат и ожидаемое значение.
+        assertThat(result, closeTo(expected, 0.1));
+    }
+}
