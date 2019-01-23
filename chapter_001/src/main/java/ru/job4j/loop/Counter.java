9db610aa9ed7716ac64d807e1085ce2134de2693
sovliv4git <sovliv4git@yandex.ru>
2019-01-23 17:10:09 +0300

    сумма всех четных чисел + тест

---
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class Counter {
+    int result = 0;
+
+    public int add(int start, int finish) {
+        for (int i = start; i <= finish; i++) {
+            if((start % 2) == 0){
+                result += start;
+            } start++;
+        }
+        return result;
+    }
+}
