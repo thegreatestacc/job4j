c38cf79beb31707176e74fa88e3ffb332d8fcae9
sovliv4git <sovliv4git@yandex.ru>
2019-01-24 15:52:21 +0300

    сделал result локальной переменной метода

--- chapter_001/src/main/java/ru/job4j/loop/Counter.java
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -1,9 +1,9 @@
 package ru.job4j.loop;
 
 public class Counter {
-    int result = 0;
 
     public int add(int start, int finish) {
+        int result = 0;
         for (int i = start; i <= finish; i++) {
             if ((start % 2) == 0) {
                 result += start;
