1a13d790810a6b3ee84f83c02d0d85f4b5b44357
sovliv4git <sovliv4git@yandex.ru>
2019-01-25 01:04:08 +0300

    исправил высоту и ширину

--- chapter_001/src/main/java/ru/job4j/loop/Board.java
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -5,8 +5,8 @@ public class Board {
     public String paint(int width, int height) {
         StringBuilder screen = new StringBuilder();
         String ln = System.lineSeparator();
-        for (int i = 1; i <= width; i++) {
-            for (int j = 1; j <= height; j++) {
+        for (int i = 1; i <= height; i++) {
+            for (int j = 1; j <= width; j++) {
                 if (((i + j) % 2) == 0) {
                     screen.append("X");
                 } else {
