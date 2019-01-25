8201da68fbf3eb01c14fc21d56af919d0fdf740c
sovliv4git <sovliv4git@yandex.ru>
2019-01-25 17:45:28 +0300

    reverse array

--- chapter_001/src/main/java/ru/job4j/array/Turn.java
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -1,4 +1,12 @@
 package ru.job4j.array;
 
 public class Turn {
+    public int[] back(int[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
+            int tmp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = tmp;
+        }
+        return array;
+    }
 }
--- chapter_001/src/test/java/ru/job4j/array/TurnTest.java
+++ chapter_001/src/test/java/ru/job4j/array/TurnTest.java
@@ -1,4 +1,26 @@
 package ru.job4j.array;
 
-public class Turn {
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class TurnTest {
+
+    @Test
+    public void whenTurnArrayWithEvenAmountOfElementsThenTurnedArray() {
+        Turn turner = new Turn();
+        int[] input = new int[] {4, 1, 6, 2};
+        int[] result = turner.back(input);
+        int[] expect = new int[] {2, 6, 1, 4};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenTurnArrayWithOddAmountOfElementsThenTurnedArray() {
+        Turn turner = new Turn();
+        int[] input = new int[] {4, 1, 6, 2, 7};
+        int[] result = turner.back(input);
+        int[] expect = new int[] {7, 2, 6, 1, 4};
+        assertThat(result, is(expect));
+    }
 }
