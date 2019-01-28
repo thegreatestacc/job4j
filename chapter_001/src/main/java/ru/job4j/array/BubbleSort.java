92a0a7835cb86253fdecd4aa066fb2ec3793702c
sovliv4git <sovliv4git@yandex.ru>
2019-01-28 14:13:38 +0300

    bubble sort

---
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+public class BubbleSort {
+
+    public int[] sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+        return arr;
+    }
+}
