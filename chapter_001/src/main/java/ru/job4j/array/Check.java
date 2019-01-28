4e6a6924a745e5b3a11d313fd34e46c3aa083a35
sovliv4git <sovliv4git@yandex.ru>
2019-01-28 13:08:10 +0300

    поправил

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -3,11 +3,12 @@ package ru.job4j.array;
 public class Check {
 
     public boolean mono(boolean[] data) {
-        boolean result = false;
+        boolean result = true;
         for (int i = 0; i < data.length; i++) {
-            if(data[1] == data[i]) {
-                result = true;
-            } else result = false;
+            if (data[0] != data[i]) {
+                result = false;
+                break;
+            }
         }
         return result;
     }
