4c2715a7d9f63b1c8fa8776d261ddab33f5ea80e
sovliv4git <sovliv4git@yandex.ru>
2019-01-28 17:25:39 +0300

    поправил

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -3,10 +3,11 @@ package ru.job4j.array;
 public class MatrixCheck {
 
     public boolean mono(boolean[][] data) {
-        boolean result = false;
+        boolean result = true;
         for (int i = 0; i < data.length - 1; i++) {
-            if ((data[i][i] == data[i + 1][i + 1]) && (data[data.length - 1][data.length - 1] == data[data.length - 1 - i][data.length - 1 - i])) {
-                result = true;
+            if ((data[i][i] != data[i + 1][i + 1])) {
+                result = false;
+                break;
             }
         }
         return result;
