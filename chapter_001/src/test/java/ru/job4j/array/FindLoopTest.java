ec0cef0bfd7f985ec377d69be8adc375862861d3
sovliv4git <sovliv4git@yandex.ru>
2019-01-28 13:13:22 +0300

    поправил

--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -19,9 +19,9 @@ public class FindLoopTest {
     public void whenWrongDecision() {
         FindLoop find = new FindLoop();
         int[] input = new int[] {4, 8, 2, 1, 0};
-        int value = 7;
+        int value = 3;
         int result = find.indexOf(input, value);
-        int expect = 2;
+        int expect = -1;
         assertThat(result, is(expect));
     }
 
