d530e8d9a9e2ac4168cda0cdf2e125cb4f96d21f
sovliv4git <sovliv4git@yandex.ru>
2019-01-28 17:33:09 +0300

    поправил

--- chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -3,8 +3,9 @@ package ru.job4j.array;
 public class BubbleSort {
 
     public int[] sort(int[] arr) {
+
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length - 1; j++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int tmp = arr[j];
                     arr[j] = arr[j + 1];
