45b4f5bb0553b64054ce165140beade0a556cd16
sovliv4git <sovliv4git@yandex.ru>
2019-02-05 14:09:31 +0300

    поправил тест

--- chapter_001/src/main/java/ru/job4j/array/ArraySort.java
+++ chapter_001/src/main/java/ru/job4j/array/ArraySort.java
@@ -1,12 +1,10 @@
 package ru.job4j.array;
 
 public class ArraySort {
-    int[] arr1 = {1, 3, 4, 6, 8};
-    int[] arr2 = {0, 2, 5, 7, 9};
-    int[] arr3 = new int[arr1.length + arr2.length];
-    int j = 0, k = 0;
 
-    public int[] sort() {
+    public int[] sort(int[] arr1, int[] arr2) {
+        int[] arr3 = new int[arr1.length + arr2.length];
+        int j = 0, k = 0;
         for (int i = 0; i < arr3.length; i++) {
             if (j < arr1.length && k < arr2.length) {
                 if (arr2[k] < arr1[j]) {
--- chapter_001/src/test/java/ru/job4j/array/ArraySortTest.java
+++ chapter_001/src/test/java/ru/job4j/array/ArraySortTest.java
@@ -5,11 +5,14 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class ArraySortTest {
+
     @Test
-    public void whenArraySort() {
+    public void whenTwoSortedArraysAreSortedAndMerged() {
         ArraySort arraySort = new ArraySort();
+        int[] input1 = {0, 2, 4, 6, 8};
+        int[] input2 = {1, 3, 5, 7, 9};
         int[] expect = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int[] result = arraySort.sort();
+        int[] result = arraySort.sort(input1, input2);
         assertThat(result, is(expect));
     }
 }
