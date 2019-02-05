eddc71c08b298ac557d13a4221cc6d04e50dc67f
sovliv4git <sovliv4git@yandex.ru>
2019-02-05 15:03:51 +0300

    поправил название типа параметра

--- chapter_002/src/main/java/ru/job4j/classes/Doctor.java
+++ chapter_002/src/main/java/ru/job4j/classes/Doctor.java
@@ -2,7 +2,7 @@ package ru.job4j.classes;
 
 public class Doctor extends Profession {
     Diagnose diagnose = new Diagnose();
-    public Diagnose heal(Pacient pacient) {
+    public Diagnose heal(Patient pacient) {
         return this.diagnose;
     }
 }
