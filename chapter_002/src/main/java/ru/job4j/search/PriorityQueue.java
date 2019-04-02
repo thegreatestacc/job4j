92e4029216523d60892a63097b978e3c5ca61197
sovliv4git <sovliv4git@yandex.ru>
2019-04-02 14:28:05 +0300

    task

--- chapter_002/src/main/java/ru/job4j/search/PriorityQueue.java
+++ chapter_002/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -1,4 +1,20 @@
 package ru.job4j.search;
 
+import java.util.LinkedList;
+
 public class PriorityQueue {
+    private LinkedList<Task> tasks = new LinkedList<>();
+    /**
+     * Метод должен вставлять в нужную позицию элемент.
+     * Позицию определять по полю приоритет.
+     * Для вставик использовать add(int index, E value)
+     * @param task задача
+     */
+    public void put(Task task) {
+        //TODO добавить вставку в связанный список.
+    }
+
+    public Task take() {
+        return this.tasks.poll();
+    }
 }
