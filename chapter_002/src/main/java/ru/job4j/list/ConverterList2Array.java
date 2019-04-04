deb0511e8f557e3242b8e24360f1aca1ef20322d
sovliv4git <sovliv4git@yandex.ru>
2019-04-04 13:32:11 +0300

    поправил

--- chapter_002/src/main/java/ru/job4j/list/ConverterList2Array.java
+++ chapter_002/src/main/java/ru/job4j/list/ConverterList2Array.java
@@ -1,20 +1,37 @@
 package ru.job4j.list;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ConverterList2Array {
     public int[][] toArray(List<Integer> list, int rows) {
         //expression to check the multiplicity of an array
-        int cells = list.size() % rows == 0 ? list.size()/rows : list.size()/rows + 1;
-        //index for checking each item
-        int index = 0;
+        int cells = list.size() % rows == 0 ? list.size() / rows : list.size() / rows + 1;
+
+        //indexes for checking each item
+        int row = 0;
+        int cell = 0;
         int[][] array = new int[rows][cells];
+
         //expression to fill in the empty cells of the array
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cells; j++) {
-                array[i][j] = index >= list.size() ? 0 : list.get(index++);
+        for (int tmp : list) {
+            array[row][cell] = tmp;
+            if (cell < cells - 1) {
+                cell++;
+            } else {
+                cell = 0;
+                row++;
             }
         }
         return array;
+    }
+    public List<Integer> convertation(List<int[]> list) {
+        List<Integer> arrList = new ArrayList<>();
+        for (int[] array : list) {
+            for (int element : array) {
+                arrList.add(element);
+            }
+        }
+        return arrList;
     }
 }
--- chapter_002/src/main/java/ru/job4j/list/ConverterMatrix2List.java
+++ chapter_002/src/main/java/ru/job4j/list/ConverterMatrix2List.java
@@ -1,7 +1,7 @@
 package ru.job4j.list;
 
 /* public List<Integer> toList (int[][] array) {} - в метод приходит двумерный массив целых чисел,
-* необходимо пройтись по всем элементам массива и добавить их в List<Integer>.*/
+ необходимо пройтись по всем элементам массива и добавить их в List<Integer>.*/
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,9 +9,10 @@ import java.util.List;
 public class ConverterMatrix2List {
     public List<Integer> toList(int array[][]) {
         List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                list.add(array[i][j]);
+
+        for (int[] rows : array) {
+            for (int cells : rows) {
+                list.add(cells);
             }
         }
         return list;
