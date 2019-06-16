3966664263442cfb4944b2a7aab10fa9bb372b33
Solovev Vladimir <sovliv@yande.ru>
2019-06-16 17:21:39 +0300

    departments

--- chapter_002/src/main/java/ru/job4j/Departaments/Departments.java
+++ chapter_002/src/main/java/ru/job4j/Departaments/Departments.java
@@ -1,7 +1,6 @@
 package ru.job4j.Departaments;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Departments {
 
@@ -29,14 +28,27 @@ public class Departments {
     }
 
     public List<Org> convert(List<String> deps) {
-        return Collections.EMPTY_LIST;
+        Set<Org> treeSet = new TreeSet();
+        List<Org> orgList = new ArrayList<>();
+        List<String> stringList = new ArrayList<>();
+        for (String str : deps) {
+            String[] strArr = str.split("/");
+            for (String str1 : strArr) {
+                stringList.add(str1);
+                treeSet.add(new Org(new ArrayList<>(stringList)));
+            }
+        }
+        orgList.addAll(treeSet);
+        return  orgList;
     }
 
     public List<Org> sortAsc(List<Org> orgs) {
+        Collections.sort(orgs);
         return orgs;
     }
 
     public List<Org> sortDesc(List<Org> orgs) {
+        Collections.sort(orgs, Collections.reverseOrder());
         return orgs;
     }
 }
