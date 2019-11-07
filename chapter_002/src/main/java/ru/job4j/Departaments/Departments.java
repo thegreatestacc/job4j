3128f8d7f1353656dad494030731a99c1538003a
Solovev Vladimir <sovliv4git@yandex.ru>
2019-11-07 09:50:26 +0300

    departments

--- chapter_002/src/main/java/ru/job4j/Departaments/Departments.java
+++ chapter_002/src/main/java/ru/job4j/Departaments/Departments.java
@@ -4,51 +4,36 @@ import java.util.*;
 
 public class Departments {
 
-    public static final class Org implements Comparable<Org> {
+    public String[] sort(String[] depart) {
+        Arrays.sort(depart);
+        return depart;
+    }
 
-        private final List<String> deps;
-
-        public Org(List<String> deps) {
-            this.deps = deps;
-        }
-
-        @Override
-        public int compareTo(Org o) {
-            int result = 0;
-            for (String str : deps) {
-                result = str.compareTo(o.toString());
+    public String[] reverseSort(String[] depart) {
+        List<String> list = new ArrayList<String>(Arrays.asList(depart));
+        list.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                int result = 0;
+                String[] mas1 = o1.split("\\\\");
+                String[] mas2 = o2.split("\\\\");
+                int minimal = Math.min(mas1.length, mas2.length);
+                for (int i = 0; i < minimal; i++) {
+                    if (!mas1[i].equals(mas2[i])) {
+                        result = mas2[i].compareTo(mas1[i]);
+                        break;
+                    }
+                }
+                if (result == 0) {
+                    if (mas1.length > mas2.length) {
+                        result = 1;
+                    } else {
+                        result = -1;
+                    }
+                }
+                return result;
             }
-            return result;
-        }
-
-        @Override
-        public String toString() {
-            return deps.toString();
-        }
-    }
-
-    public List<Org> convert(List<String> deps) {
-        Set<Org> treeSet = new TreeSet();
-        List<Org> orgList = new ArrayList<>();
-        List<String> stringList = new ArrayList<>();
-        for (String str : deps) {
-            String[] strArr = str.split("/");
-            for (String str1 : strArr) {
-                stringList.add(str1);
-                treeSet.add(new Org(new ArrayList<>(stringList)));
-            }
-        }
-        orgList.addAll(treeSet);
-        return  orgList;
-    }
-
-    public List<Org> sortAsc(List<Org> orgs) {
-        Collections.sort(orgs);
-        return orgs;
-    }
-
-    public List<Org> sortDesc(List<Org> orgs) {
-        Collections.sort(orgs, Collections.reverseOrder());
-        return orgs;
+        });
+        return list.toArray(new String[list.size()]);
     }
 }
--- chapter_002/src/test/java/ru/job4j/Departaments/DepartmentsTest.java
+++ chapter_002/src/test/java/ru/job4j/Departaments/DepartmentsTest.java
@@ -1,48 +1,65 @@
 package ru.job4j.Departaments;
 
 import org.junit.Test;
-import java.util.Arrays;
-import java.util.List;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class DepartmentsTest {
-
     @Test
-    public void whenMissed() {
-        Departments deps = new Departments();
-        List<String> input = Arrays.asList("k1/sk1");
-        List<Departments.Org> expect = Arrays.asList(
-                new Departments.Org(Arrays.asList("k1")),
-                new Departments.Org(Arrays.asList("k1", "sk1"))
-        );
-        List<Departments.Org> result = deps.convert(input);
-        assertThat(result, is(expect));
+    public void sort() {
+        String[] depart = {
+                "K1\\SK1",
+                "K1\\SK2",
+                "K1\\SK1\\SSK1",
+                "K1\\SK1\\SSK2",
+                "K2",
+                "K1",
+                "K2\\SK1",
+                "K2\\SK1\\SSK1",
+                "K2\\SK1\\SSK2",
+        };
+        String[] expected = {
+                "K1",
+                "K1\\SK1",
+                "K1\\SK1\\SSK1",
+                "K1\\SK1\\SSK2",
+                "K1\\SK2",
+                "K2",
+                "K2\\SK1",
+                "K2\\SK1\\SSK1",
+                "K2\\SK1\\SSK2"
+        };
+
+        Departments sortdep = new Departments();
+        String[] result = sortdep.sort(depart);
+        assertThat(result, is(expected));
     }
-
     @Test
-    public void whenAsc() {
-        Departments deps = new Departments();
-        List<String> input = Arrays.asList("k1/sk1", "k2");
-        List<Departments.Org> expect = Arrays.asList(
-                new Departments.Org(Arrays.asList("k1")),
-                new Departments.Org(Arrays.asList("k1", "sk1")),
-                new Departments.Org(Arrays.asList("k2"))
-        );
-        List<Departments.Org> result = deps.sortAsc(deps.convert(input));
-        assertThat(result, is(expect));
-    }
-
-    @Test
-    public void whenDesc() {
-        Departments deps = new Departments();
-        List<String> input = Arrays.asList("k1/sk1", "k2");
-        List<Departments.Org> expect = Arrays.asList(
-                new Departments.Org(Arrays.asList("k2")),
-                new Departments.Org(Arrays.asList("k1")),
-                new Departments.Org(Arrays.asList("k1", "sk1"))
-        );
-        List<Departments.Org> result = deps.sortDesc(deps.convert(input));
-        assertThat(result, is(expect));
+    public void reverseSort() {
+        String[] depart = {
+                "K1\\SK1",
+                "K1\\SK2",
+                "K1\\SK1\\SSK1",
+                "K1\\SK1\\SSK2",
+                "K2",
+                "K2\\SK1\\SSK1",
+                "K2\\SK1\\SSK2",
+                "K1",
+                "K2\\SK1",
+        };
+        String[] expected = {
+                "K2",
+                "K2\\SK1",
+                "K2\\SK1\\SSK2",
+                "K2\\SK1\\SSK1",
+                "K1",
+                "K1\\SK2",
+                "K1\\SK1",
+                "K1\\SK1\\SSK2",
+                "K1\\SK1\\SSK1"
+        };
+        Departments sortdepreverse = new Departments();
+        String[] result = sortdepreverse.reverseSort(depart);
+        assertThat(result, is(expected));
     }
 }
