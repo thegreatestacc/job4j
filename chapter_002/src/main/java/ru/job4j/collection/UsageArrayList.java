f31bcadc78015a1122ec16d3d68c0ad3e3bd5f81
thegreatestacc <thegreatestemailname@gmail.com>
2020-09-21 16:30:14 +0300

    collection arraylist

---
+++ chapter_002/src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -0,0 +1,17 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsageArrayList {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Petr");
+        list.add("Ivan");
+        list.add("Stepan");
+
+        for (String str : list) {
+            System.out.println(str);
+        }
+    }
+}
