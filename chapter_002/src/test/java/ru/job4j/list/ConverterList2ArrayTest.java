4ef027c9cedce7a9c98e714c5e18be16098874c0
thegreatestacc <thegreatestemailname@gmail.com>
2020-09-23 12:04:31 +0300

    заменить создание коллекции на listof

--- chapter_002/src/test/java/ru/job4j/list/ConverterList2ArrayTest.java
+++ chapter_002/src/test/java/ru/job4j/list/ConverterList2ArrayTest.java
@@ -1,16 +1,17 @@
 package ru.job4j.list;
 
 import org.junit.Test;
+
+import java.util.List;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-
-import java.util.Arrays;
 
 public class ConverterList2ArrayTest {
     @Test
     public void when7ElementsThen9() {
         ConverterList2Array list = new ConverterList2Array();
-        int[][] result = list.toArray(Arrays.asList(1, 2, 3, 4, 5, 6, 7), 3);
+        int[][] result = list.toArray(List.of(1, 2, 3, 4, 5, 6, 7), 3);
         int[][] except = {
                 {1, 2, 3},
                 {4, 5, 6},
--- chapter_002/src/test/java/ru/job4j/list/ConverterMatrix2ListTest.java
+++ chapter_002/src/test/java/ru/job4j/list/ConverterMatrix2ListTest.java
@@ -15,7 +15,7 @@ public class ConverterMatrix2ListTest {
             {1, 2},
             {3, 4},
         };
-        List<Integer> expect = Arrays.asList(1, 2, 3, 4);
+        List<Integer> expect = List.of(1, 2, 3, 4);
         List<Integer> result = list.toList(input);
         assertThat(result, is(expect));
     }
